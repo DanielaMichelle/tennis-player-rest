ce47f18f9cca33495a95e251a8c771f9c7e3702c
Daniela Infantes <danielamij251@gmail.com>
2023-12-13 14:54:20 -0500

    @PathVariable, findById() method to get a player by id.

--- src/main/java/io/datajek/tennisplayerrest/PlayerService.java
+++ src/main/java/io/datajek/tennisplayerrest/PlayerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -17,6 +18,21 @@ public class PlayerService {
     }
 
     //Get player by ID
+    public Player getPlayer(int id) {
+
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        Player p = null;
+
+        //if the Optional has a value, assign it to p
+        if(tempPlayer.isPresent())
+            p = tempPlayer.get();
+            //if value is not found, throw a runtime exception
+        else
+            throw new RuntimeException("Player with id "+ id + " not found.");
+
+        return p;
+    }
 
     //Add a player
 
