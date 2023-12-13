d3b2f261f01fe7ebebe62adb08712129d3feb8f9
Daniela Infantes <danielamij251@gmail.com>
2023-12-13 14:39:12 -0500

    @GetMapping, getAll() to get a list of all the players.

--- src/main/java/io/datajek/tennisplayerrest/PlayerService.java
+++ src/main/java/io/datajek/tennisplayerrest/PlayerService.java
@@ -3,12 +3,18 @@ package io.datajek.tennisplayerrest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlayerService {
     @Autowired
     private PlayerRepository repo;
 
     //Get all players
+    public List<Player> getAllPlayers() {
+        //call repository method
+        return repo.findAll();
+    }
 
     //Get player by ID
 
